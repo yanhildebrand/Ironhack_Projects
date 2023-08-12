6b9d33a4428aa94b2e79071054734361bf806502
yanhildebrand <hildebrand.yan@gmail.com>
2023-08-12 13:55:10 +0200

    2023.08.12_1

---
+++ Project_2_spotify_recommender/config.py
@@ -0,0 +1,7 @@
+# 02
+client_id = "7109a639a9904008887228a48e5b2d16"
+client_secret = "22bc8801a21d497ea798338bfd1b8e06"
+
+# 01
+# client_id = "724eebf613ac4ded8c69ccd83fa432a4"
+# client_secret = "953a11d85b514798a37f66b561f22baa"
